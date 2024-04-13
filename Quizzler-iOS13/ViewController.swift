197d07f0de5c030e4dbfe13b19024c2c25ded703
Stavros <snikoletopoulos@hotmail.com>
2024-04-14 02:03:34 +0300

    Add more questions from readme file

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -7,9 +7,18 @@ class ViewController: UIViewController {
     @IBOutlet var falseButton: UIButton!
 
     let questions = [
-        Question("Four + Two is equal to Six.", answer: "True"),
-        Question("Five - Three is equal to One.", answer: "True"),
-        Question("Three + Eight is equal to Ten.", answer: "False"),
+        Question("A slug's blood is green.", answer: "True"),
+        Question("Approximately one quarter of human bones are in the feet.", answer: "True"),
+        Question("The total surface area of two human lungs is approximately 70 square metres.", answer: "True"),
+        Question("In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answer: "True"),
+        Question("In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answer: "False"),
+        Question("It is illegal to pee in the Ocean in Portugal.", answer: "True"),
+        Question("You can lead a cow down stairs but not up stairs.", answer: "False"),
+        Question("Google was originally called 'Backrub'.", answer: "True"),
+        Question("Buzz Aldrin's mother's maiden name was 'Moon'.", answer: "True"),
+        Question("The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answer: "False"),
+        Question("No piece of square dry paper can be folded in half more than 7 times.", answer: "False"),
+        Question("Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answer: "True"),
     ]
     var activeQuestion = 0
 
